365ea1f51a22674c9e1f43eae81683fcb7acf57c
Bill Leddy <bill@williesworkshop.net>
2019-04-22 20:20:05 -0700

    a bit more calendar

--- staffing/views/calendar.py
+++ staffing/views/calendar.py
@@ -19,21 +19,43 @@ def setExits():
     # g.deleteURL = url_for('.delete')
     g.title = 'Calendar'
 
-
-@mod.route('calendar',methods=['GET','POST',])
-@mod.route('calendar/',methods=['GET','POST',])
-def display():
+@mod.route('calendar/<int:month>/')
+@mod.route('calendar/<int:month>')
+@mod.route('calendar/<int:month>/<int:year>/')
+@mod.route('calendar/<int:month>/<int:year>')
+@mod.route('calendar')
+@mod.route('calendar/')
+def display(month=None,year=None):
     setExits()
     site_config = get_site_config()
-    g.title="{} Calendar".format(site_config.get('ORG_NAME','Our'))
+    g.title="Calendar"
     
     start_date = local_datetime_now()
     start_date = date(start_date.year,start_date.month,1)
-    if request.form:
-        try:
-            start_date = date(int(request.form['year']),int(request.form['month']),1)
-        except:
-            pass # use todays date
+    if month == None:
+        month = start_date.month
+    if year == None:
+        year = start_date.year
+        
+    month = cleanRecordID(month)
+    year = cleanRecordID(year)
+    refresh = False
+    
+    if year < 1951 or year > 3000:
+        year = start_date.year
+        refresh = True
+        
+    if month < 1 or month > 12:
+        month = start_date.month
+        refresh = True
+        
+    if refresh:
+        return redirect(url_for('.display')+"{}/{}".format(month,year))
+            
+    try:
+        start_date = date(year,month,1)
+    except:
+        pass # use todays date
             
     end_date = start_date + timedelta(days=calendar.monthrange(start_date.year,start_date.month)[1])
     
@@ -41,7 +63,7 @@ def display():
     #import pdb;pdb.set_trace()
         
     job_data = get_job_rows(start_date,end_date,is_admin=True,
-            job_status_where=" and lower(job.status) = 'public calendar' ",
+            job_status_where=get_job_status_where(),
             order_by="date(job.start_date,'localtime')")
             
     job_list_dict = {}
@@ -72,5 +94,53 @@ def display():
                 cal_list[week][day] = cal_list[week][day] + (job_list_dict[cal_list[week][day][cal_day]],)
             else:
                 cal_list[week][day] = cal_list[week][day] + ([],)
+        
+    if month == 1:
+        #go back to Dec of last year
+        last_month = date(start_date.year-1,12,1)
+        #go to Feb of this year
+        next_month = date(start_date.year,2,1)
+    elif month == 12:
+        #go to Nov. of this year
+        last_month = date(start_date.year,11,1)
+        #go to Jan of next year
+        next_month = date(start_date.year+1,1,1)
+    else:
+        # year does not change
+        next_month = date(start_date.year,start_date.month+1,1)
+        last_month = date(start_date.year,start_date.month-1,1)
+    
+    return render_template('calendar.html',
+                            job_data=job_data,
+                            calendar=calendar,
+                            cal_list=cal_list,
+                            start_date=start_date,
+                            last_month=last_month,
+                            next_month=next_month,
+                            )
 
-    return render_template('calendar.html',job_data=job_data,calendar=calendar,cal_list=cal_list,start_date=start_date)
+
+@mod.route('calendar/event/<int:job_id>')
+@mod.route('calendar/event/<int:job_id>/')
+@mod.route('calendar/event')
+@mod.route('calendar/event/')
+def event(job_id=None):
+    """Return a page with the event associated with the job"""
+    
+    #import pdb;pdb.set_trace()
+    
+    job_id = cleanRecordID(job_id)
+    if job_id < 1:
+        return redirect(url_for('.display'))
+        
+    job = get_job_rows(None,None,is_admin=True,where="job.id = {}".format(job_id),
+                        job_status_where=get_job_status_where(),
+                        )
+    if not job:
+        return redirect(url_for('.display'))
+    
+    return render_template('calendar_event.html',job=job[0])
+    
+    
+def get_job_status_where():
+    return " and lower(job.status) = 'public calendar' "
