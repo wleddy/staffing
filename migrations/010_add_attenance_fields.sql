7550e81ed41ca60816e3b20c74e5ff8c8d4e33f8
Bill Leddy <bill@williesworkshop.net>
2019-12-23 16:22:10 -0800

    set values for existing records

--- migrations/010_add_attenance_fields.sql
+++ migrations/010_add_attenance_fields.sql
@@ -1,3 +1,5 @@
 .bail on
 ALTER TABLE attendance ADD COLUMN input_by NUMBER;
 ALTER TABLE attendance ADD COLUMN input_date DATETIME;
+
+UPDATE OR ROLLBACK attendance SET input_by = 'Jeremiah Rohr', input_date = datetime('now') where start_date is not null;
