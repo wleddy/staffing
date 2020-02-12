179b15e8b5e5f59bb6c023c7d54e5b5cb4bb5f86
Bill Leddy <bill@williesworkshop.net>
2020-02-12 14:39:13 -0800

    fix sql

--- migrations/010_add_attenance_fields.sql
+++ migrations/010_add_attenance_fields.sql
@@ -1,5 +1,7 @@
 .bail on
-ALTER TABLE attendance ADD COLUMN input_by NUMBER;
+ALTER TABLE attendance ADD COLUMN input_by TEXT;
 ALTER TABLE attendance ADD COLUMN input_date DATETIME;
 
 UPDATE OR ROLLBACK attendance SET input_by = 'Jeremiah Rohr', input_date = datetime('now') where start_date is not null;
+
+.bail off
