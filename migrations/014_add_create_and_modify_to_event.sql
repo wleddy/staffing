f671a678f06ece16208dafc1dec44cf59679953d
Bill Leddy <bill@williesworkshop.net>
2020-03-09 09:49:47 -0700

    change default date

--- migrations/014_add_create_and_modify_to_event.sql
+++ migrations/014_add_create_and_modify_to_event.sql
@@ -6,6 +6,6 @@ ALTER TABLE event ADD COLUMN created DATETIME;
 ALTER TABLE event ADD COLUMN modified DATETIME;
 
 -- add a default date
-UPDATE event SET created = datetime('now'), modified = datetime('now');
+UPDATE event SET created = datetime('2020-03-08 00:00:00'), modified = datetime('2020-03-08 00:00:00');
 
 .bail off
